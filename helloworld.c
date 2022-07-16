7396f479671add9ae1d5026814d457d933a01083
gevera1 <gevero.adrianjames@gmail.com>
2022-07-16 11:32:09 -0700

    Fixed for loop in helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -4,7 +4,7 @@
 int main( void )
 {
         printf("Hello World!\n");
-        for (int 80 = 0; i < 100; i++)
+        for (int i = 80; i < 100; i++)
         {
                 printf("%c\n", i);
         }
